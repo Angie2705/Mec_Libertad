442959d087d79547046572c5c6d7b86518b1653e
Jorge <jor.saezb@duocuc.cl>
2024-10-01 13:04:10 -0300

    Responsive Nosotros

--- Mecanica Libertad/src/components/Header.jsx
+++ Mecanica Libertad/src/components/Header.jsx
@@ -2,87 +2,83 @@ import React, { useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { Link, NavLink } from 'react-router-dom'
 
-const Header = () =>{
+const Header = () => {
 
-    const [showNavBar, setShowNavBar] = useState(true);
-    const [lastScrollY, setLastScrollY] = useState(0);
+  const [showNavBar, setShowNavBar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
-    const controlNavBar = () =>{
-      if (typeof window != 'undefined') {
-        if (window.scrollY < lastScrollY) {
-          setShowNavBar(true)
-        } else{
-          setShowNavBar(false)
-        }
-  
-        setLastScrollY(window.scrollY)
+  const controlNavBar = () => {
+    if (typeof window != 'undefined') {
+      if (window.scrollY < lastScrollY) {
+        setShowNavBar(true)
+      } else {
+        setShowNavBar(false)
       }
+
+      setLastScrollY(window.scrollY)
     }
-    
-    useEffect(()=>{
+  }
 
-      if (typeof window != 'undefined') {
-        window.addEventListener('scroll',controlNavBar)
+  useEffect(() => {
 
-        return () =>{
-          window.removeEventListener('scroll', controlNavBar)
-        };
-      }
-   
-    }, [lastScrollY]);
+    if (typeof window != 'undefined') {
+      window.addEventListener('scroll', controlNavBar)
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
-
-    const toogleMenu = () =>{
-      setIsMenuOpen(!isMenuOpen)
+      return () => {
+        window.removeEventListener('scroll', controlNavBar)
+      };
     }
-    
-    return(
-        <nav className={`bg-white shadow-md md:py-4 transition-transform duration-700 ${showNavBar ? 'translate-y-0' : '-translate-y-full'}`}>
-            <div className="flex items-center">
-              
-              <div className="md:absolute pl-5">
-                <a href="">
-                  <img className=" " src={assets.logo} alt="" />
-                </a>
-              </div>
 
-              <div className="absolute end-5 top-5">
-                <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex md:hidden p-2 w-10 h-10 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 
+
+  }, [lastScrollY]);
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toogleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+  return (
+    <nav className={`bg-white shadow-md md:py-4 transition-transform duration-700 ${showNavBar ? 'translate-y-0' : '-translate-y-full'}`}>
+      <div className="flex items-center">
+
+        <div className="md:absolute pl-5">
+          <a href="">
+            <img className=" " src={assets.logo} alt="" />
+          </a>
+        </div>
+
+        <div className="absolute end-5 top-5">
+          <button data-collapse-toggle="navbar-sticky" type="button" class=" items-center justify-center inline-flex md:hidden p-2 w-10 h-10 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 
                   dark:text-black dark:hover:bg-gray-500 dark:focus:ring-gray-500" aria-controls="navbar-sticky" aria-expanded="false" onClick={toogleMenu}>
-                  <svg class="w-5 h-5" aria-hidden="true" fill="none" viewBox="0 0 17 14">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-                  </svg>
-                </button>            
-              </div>
+            <svg class="w-5 h-5" aria-hidden="true" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+          </button>
+        </div>
 
-              <ul class="hidden md:flex w-full justify-center md:gap-x-8 bg-white text-gray-900 font-semibold ">
-                <li className="hover:bg-gray-300 px-4 py-2 rounded-md"> <a className="text-sm " href="">Inicio</a> </li>
-                <li className="hover:bg-gray-300 px-4 py-2 rounded-md"> <a className="text-sm" href="">Nosotros</a> </li>
-                <li className="hover:bg-gray-300 px-4 py-2 rounded-md"> <a className="text-sm" href="">Servicios</a> </li>
-                <li className="hover:bg-gray-300 px-4 py-2 rounded-md">
-                  <NavLink to='/producto'>
-                    <a className="text-sm font-semibold" href="">Productos</a>
-                  </NavLink>
-                </li>
-              </ul> 
-            </div>
+        <ul class="hidden md:flex w-full justify-center md:gap-x-8 bg-white text-gray-900 font-semibold text-sm lg:text-base">
+          <li className="hover:bg-gray-300 px-4 py-2 rounded-md"> <a href="">Inicio</a> </li>
+          <li className="hover:bg-gray-300 px-4 py-2 rounded-md"> <a href="">Nosotros</a> </li>
+          <li className="hover:bg-gray-300 px-4 py-2 rounded-md"> <a href="">Servicios</a> </li>
+          <li className="hover:bg-gray-300 px-4 py-2 rounded-md">
+            <NavLink to='/producto'> <a href="">Productos</a> </NavLink>
+          </li>
+        </ul>
+      </div>
 
-            {isMenuOpen ? (
-              <ul class="flex-col md:hidden pl-8 bg-white text-gray-900" >
-                <li className="font-semibold py-3 px-6 hover:bg-gray-300 rounded-md"> <a className="text-sm" href="">Inicio</a> </li>
-                <li className="font-semibold py-3 px-6 hover:bg-gray-300 rounded-md"> <a className="text-sm" href="">Nosotros</a> </li>
-                <li className="font-semibold py-3 px-6 hover:bg-gray-300 rounded-md"> <a className="text-sm" href="">Servicios</a> </li>
-                <li className="font-semibold py-3 px-6 hover:bg-gray-300 rounded-md">
-                  <NavLink to='/producto'>
-                    <a className="text-sm font-semibold" href="">Productos</a>
-                  </NavLink>
-                </li>
-              </ul> 
-            ):null}
+      {isMenuOpen ? (
+        <ul id="navlinks" className="transition-all duration-700 ease-in-out flex-col absolute w-full md:hidden px-20 text-center bg-white text-gray-900 font-semibold text-sm">
+          <li className="h-6 px-6 hover:bg-gray-300 rounded-md"> <a href="">Inicio</a> </li>
+          <li className="py-3 px-6 hover:bg-gray-300 rounded-md"> <a href="">Nosotros</a> </li>
+          <li className="py-3 px-6 hover:bg-gray-300 rounded-md"> <a href="">Servicios</a> </li>
+          <li className="py-3 px-6 hover:bg-gray-300 rounded-md">
+            <NavLink to='/producto'> <a href="">Productos</a> </NavLink>
+          </li>
+        </ul>
+      ) : null}
 
-          </nav>
-    );
+    </nav>
+  );
 }
 
 export default Header;
--- Mecanica Libertad/src/components/Nosotros.jsx
+++ Mecanica Libertad/src/components/Nosotros.jsx
@@ -3,39 +3,35 @@ import { assets } from '../assets/assets';
 import Title from './Title';
 function Nosotros() {
   return (
-    
-    
-    <section id="nosotros" className=" mt-3 
-         
-   ">
+
+
+    <section id="nosotros">
       <h2 className="w-full text-center text-4xl font-bold text-gray-800 p-6 ">Sobre Nosotros</h2>
       <div className='grid grid-cols-1 lg:grid-cols-3 gap-x-4 lg:gap-y-0 gap-y-8 p-2'>
-      <div className="flex flex-col text-left items-center justify-center  rounded-lg md:py-5 bg-red-800 ml-16 col-span-2" >
-        
-        <p className="text-md text-white md:px-6 lg:px-12 lg:py-6 word-space rounded-lg text-justify" style={{wordSpacing:'8px'}}>
-        Mecánica Libertad nace el año 2013 en la cuidad de Santiago de Chile, luego de que sus fundadores, Wilson Ocas Velásquez padre y Wilson Ocas Valverde, de profesión mecánico en máquinas y herramientas, tomaran la decisión de emprender, dedicando todo su tiempo y conocimientos técnicos adquiridos durante sus 11 años de vida laboral desarrollada tanto en Perú como en Chile en el área de la Metalmecánica.<br/>
-        Al comienzo la empresa contaba con sólo tres máquinas, operadas por su propio dueño y dos personas más, dándole solución a temas de mecanizado y estructuras, pero en pequeñas cantidades.<br/>
-        Debido a la gran calidad de sus trabajos, Mecánica Libertad adquirió cada vez más prestigio en la industria y por ende mayor cantidad de clientes, lo que derivó en un gran crecimiento en el corto plazo, adquiriendo maquinaria de última generación y especializada en la industria que permite la realización de los trabajos con una excelente calidad, precisión de las medidas y en los tiempos que nuestros clientes requieren.<br/>
-        Hoy en día nuestra empresa cuenta con colaboradores con sólidos conocimientos en las áreas que se desenvuelven, los cuales gracias a su esfuerzo y compromiso hacen de Mecánica Libertad una empresa dedicada a dar soluciones a la industria, destacando siempre la calidad, seriedad y compromiso de nuestros trabajos, lo que nos ha permitido trabajar con clientes de renombre.
-        <br/><br/>
-        <span className='font-bold text-lg font-serif italic'>Nuestra Visión: </span>
-         “Convertirnos en la mejor alternativa para la industria en ámbitos metalmecánicos”.
-        <br/><br/>
-        <span className='font-bold text-lg font-serif italic'>Nuestra Misión: </span>
-         "Desarrollar soluciones de calidad, innovadoras, rentables y que cumplan con todo lo que nuestos clientes necesiten. Establecer relaciones de confianza con nuestros clientes, las cuales sienten sus bases en la calidad de nuestros colaboradores y la buena comunicación".
-        </p>
+        <div className="flex flex-col items-center justify-center rounded-lg py-5 bg-red-800 mx-10 lg:ml-12 lg:mr-0 col-span-2">
+
+          <p className="text-md text-white px-6 word-space rounded-lg text-justify" style={{ wordSpacing: '8px' }}>
+            Mecánica Libertad nace el año 2013 en la cuidad de Santiago de Chile, luego de que sus fundadores, Wilson Ocas Velásquez padre y Wilson Ocas Valverde, de profesión mecánico en máquinas y herramientas, tomaran la decisión de emprender, dedicando todo su tiempo y conocimientos técnicos adquiridos durante sus 11 años de vida laboral desarrollada tanto en Perú como en Chile en el área de la Metalmecánica.<br />
+            Al comienzo la empresa contaba con sólo tres máquinas, operadas por su propio dueño y dos personas más, dándole solución a temas de mecanizado y estructuras, pero en pequeñas cantidades.<br />
+            Debido a la gran calidad de sus trabajos, Mecánica Libertad adquirió cada vez más prestigio en la industria y por ende mayor cantidad de clientes, lo que derivó en un gran crecimiento en el corto plazo, adquiriendo maquinaria de última generación y especializada en la industria que permite la realización de los trabajos con una excelente calidad, precisión de las medidas y en los tiempos que nuestros clientes requieren.<br />
+            Hoy en día nuestra empresa cuenta con colaboradores con sólidos conocimientos en las áreas que se desenvuelven, los cuales gracias a su esfuerzo y compromiso hacen de Mecánica Libertad una empresa dedicada a dar soluciones a la industria, destacando siempre la calidad, seriedad y compromiso de nuestros trabajos, lo que nos ha permitido trabajar con clientes de renombre.
+            <br /><br />
+            <span className='font-bold text-lg font-serif italic'>Nuestra Visión: </span>
+            “Convertirnos en la mejor alternativa para la industria en ámbitos metalmecánicos”.
+            <br /><br />
+            <span className='font-bold text-lg font-serif italic'>Nuestra Misión: </span>
+            "Desarrollar soluciones de calidad, innovadoras, rentables y que cumplan con todo lo que nuestos clientes necesiten. Establecer relaciones de confianza con nuestros clientes, las cuales sienten sus bases en la calidad de nuestros colaboradores y la buena comunicación".
+          </p>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 gap-5 lg:gap-6 h-auto p-6 lg:p-7 bg-gray-200 rounded-lg pb-5 ml-8 mr-4 lg:ml-0 lg:mr-6">
+          {/* <img className="w-full h-full object-cover rounded-lg hover:scale-95 transition duration-1000 cursor-pointer" src={assets.Mecanica2} alt="" />
+          <img className="w-full h-full object-cover rounded-lg hover:scale-95 transition duration-1000 cursor-pointer" src={assets.Mecanica3} alt="" /> */}
+          <img className="w-full h-full object-cover rounded-lg hover:scale-95 transition duration-1000 cursor-pointer" src={assets.Mecanica4} alt="" />
+          <img className="w-full h-full object-cover rounded-lg hover:scale-95 transition duration-1000 cursor-pointer" src={assets.Mecanica5} alt="" />
+        </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 gap-5 lg:gap-10 h-screen sm:h-auto p-2 md:p-6 lg:p-8 bg-gray-200 rounded-lg pb-5">
-  {/* <img className="w-full h-full object-cover rounded-lg hover:scale-95 transition duration-1000 cursor-pointer" src={assets.Mecanica2} alt="" />
-  <img className="w-full h-full object-cover rounded-lg hover:scale-95 transition duration-1000 cursor-pointer" src={assets.Mecanica3} alt="" /> */}
-  <img className="w-full h-full object-cover rounded-lg hover:scale-95 transition duration-1000 cursor-pointer" src={assets.Mecanica4} alt="" />
-  <img className="w-full h-full object-cover rounded-lg hover:scale-95 transition duration-1000 cursor-pointer" src={assets.Mecanica5} alt="" />
-</div>
-
-
-      </div>
-      
     </section>
   );
 
