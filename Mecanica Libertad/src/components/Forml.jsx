3f49160a12949e9af328cf7eece75356b05ce56e
Jorge <jor.saezb@duocuc.cl>
2024-10-22 13:21:54 -0300

    Validación Login

--- Mecanica Libertad/src/components/Forml.jsx
+++ Mecanica Libertad/src/components/Forml.jsx
@@ -2,63 +2,217 @@ import React, { useEffect, useState } from 'react';
 import { app, auth } from '../firebase';
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
-
+import { useForm } from "react-hook-form";
 
 
 const Forml = () => {
 
     const navigate = useNavigate();
-    const [register, setRegister] = useState(false);
+    const [regis, setRegister] = useState(false);
     console.log(auth)
-    const functionAuth = async (e)=>{
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const functionAuth = async (e) => {
         e.preventDefault();
+        
         const email = e.target.email.value;
         const password = e.target.password.value;
 
         console.log(auth)
         console.log("e")
-        
-        if (register) {
+
+        if (regis) {
             await createUserWithEmailAndPassword(auth, email, password)
-        } else{
+        } else {
             await signInWithEmailAndPassword(auth, email, password)
             navigate('/')
         }
     }
 
 
-
-
-    
     return (
-  
-            <form onSubmit={functionAuth} className=' mt-10 space-y-4 md:space-y-6' >
 
-                <div>
-                    <label className='font-medium'>Correo Electrónico</label>
-                    <input type="email" className="w-full border-2 border-gray-400 rounded-xl p-2 mt-1" name="correo_actual" id="email"
-                        placeholder="example@example.com" />
-                </div>
+        <form onSubmit={handleSubmit(functionAuth)} className=' mt-10 space-y-4 md:space-y-6' >
 
-                <div>
-                    <label className='font-medium'>Contraseña</label>
-                    <input type="password" className="w-full border-2 border-gray-400 rounded-xl p-2 mt-1" name="contrasena_actual" id="password"
-                        placeholder="Ingrese su Contraseña" />
-                </div>
+            <div>
+                <label className='font-medium'>Correo Electrónico</label>
+                <input  className="w-full border-2 border-gray-400 rounded-xl p-2 mt-1" name="email" id="email"
+                    placeholder="example@example.com" {...register("email",{
+                        required :{
+                            value: true,
+                            message: "El campo es requerido"
+                        },
+                        pattern:{
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "El formato no es correcto"
+                        }
+                    })}/>
+                {errors.email && <span className='text-red-600'>{errors.email.message}</span>}
+            </div>
 
-                <div className='flex justify-center items-center'>
-                    <div id="error" class="alert alert-danger"></div>
-                    <button type="submit" className="px-4 py-2 text-base rounded-full bg-red-600 text-white hover:bg-red-700">{register ? "Registrate" : "Iniciar sesión"}</button>
-                    <h4>{register ? "Si ya tienes cuenta: " : "Si no tienes cuenta:"}
-                        <button onClick={()=>setRegister(!register)} className='px-1  text-base rounded-full bg-gray-600 text-white hover:bg-red-700'>{register ? "Inicia sesión" : "Registrate"}</button>
-                    </h4>
-                    
-                </div>
+            <div>
+                <label className='font-medium'>Contraseña</label>
+                <input type="password" className="w-full border-2 border-gray-400 rounded-xl p-2 mt-1" name="password" id="password"
+                    placeholder="Ingrese su Contraseña" {...register("password",{
+                        required :{
+                            value: true,
+                            message: "El campo es requerido"
+                        },
+                    })}/>
+                {errors.password && <span className='text-red-600'>{errors.password.message}</span>}
+                
+            </div>
 
-            </form>
+            <div className='flex justify-center items-center'>
+                <button type="submit" className="px-4 py-2 text-base rounded-full bg-red-600 text-white hover:bg-red-700">{regis ? "Registrate" : "Iniciar sesión"}</button>
+                <h4>{regis ? "Si ya tienes cuenta: " : "Si no tienes cuenta:"}
+                    <button onClick={() => setRegister(!regis)} className='px-1 text-base rounded-full bg-gray-600 text-white hover:bg-red-700'>{regis ? "Inicia sesión" : "Registrate"}</button>
+                </h4>
 
+            </div>
+
+        </form>
 
     )
 }
 
 export default Forml
+
+/*<form onSubmit={functionAuth} className=' mt-10 space-y-4 md:space-y-6' >*/
+
+/*correo: /^[a-zA-Z0-9. _%+-]+@[a-zA-Z0-9 */
+
+/*
+-----------------------------------------------------------
+const navigate = useNavigate();
+    const [register, setRegister] = useState(false);
+    console.log(auth)
+
+    const functionAuth = async (e) => {
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        console.log(auth)
+        console.log("e")
+
+        if (register) {
+            await createUserWithEmailAndPassword(auth, email, password)
+        } else {
+            await signInWithEmailAndPassword(auth, email, password)
+            navigate('/')
+        }
+    }
+
+const form = {};
+
+    const [errors, setErrors] = useState([]);
+
+    const handleSubmit =(event) => {
+        event.preventDefault()
+        const err = onValidate(form)
+
+        if (err == null){
+            console.log("Enviando") 
+        } else {
+            setErrors(err)
+        }
+    }
+
+    const onValidate = (form) => {
+        let isErrors = false;
+        let errors = {};
+
+        if (!form.correo) {
+            errors.correo = 'El campo "Correo Electronico" no puede estar vacío.'
+            isErrors = true
+        }
+
+        if (!form.contrasena) {
+            errors.contrasena = 'El campo "Contraseña" no puede estar vacío.'
+            isErrors = true
+        }
+
+        return isErrors ? errors : null
+
+    }
+
+    return (
+
+        <form onSubmit={handleSubmit} className=' mt-10 space-y-4 md:space-y-6' >
+
+            <div>
+                <label className='font-medium'>Correo Electrónico</label>
+                <input type="email" className="w-full border-2 border-gray-400 rounded-xl p-2 mt-1" name="correo_actual" id="email"
+                    placeholder="example@example.com" value={form.correo}/>
+                {errors.correo && <div className="alert alert-danger p-1">{errors.correo}</div>}
+            </div>
+
+            <div>
+                <label className='font-medium'>Contraseña</label>
+                <input type="password" className="w-full border-2 border-gray-400 rounded-xl p-2 mt-1" name="contrasena_actual" id="password"
+                    placeholder="Ingrese su Contraseña" />
+                {errors.contrasena && <div className="alert alert-danger p-1">{errors.contrasena}</div>}
+            </div>
+
+            <div className='flex justify-center items-center'>
+                <button type="submit" className="px-4 py-2 text-base rounded-full bg-red-600 text-white hover:bg-red-700">{register ? "Registrate" : "Iniciar sesión"}</button>
+                <h4>{register ? "Si ya tienes cuenta: " : "Si no tienes cuenta:"}
+                    <button onClick={() => setRegister(!register)} className='px-1  text-base rounded-full bg-gray-600 text-white hover:bg-red-700'>{register ? "Inicia sesión" : "Registrate"}</button>
+                </h4>
+
+            </div>
+
+        </form> 
+        
+        
+        
+-------------------------------------------------------------------------------    
+        
+        
+        
+<form onSubmit={functionAuth} className=' mt-10 space-y-4 md:space-y-6' >
+
+            <div>
+                <label className='font-medium'>Correo Electrónico</label>
+                <input type="email" className="w-full border-2 border-gray-400 rounded-xl p-2 mt-1" name="correo_actual" id="email"
+                    placeholder="example@example.com"
+                    {...register("correo",{
+                        required :{
+                            value: true,
+                            message: "El campo es requerido"
+                        },
+                        pattern:{
+                            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                            message: "El formato no es correcto"
+                        }
+                    })}/>
+            </div>
+
+            <div>
+                <label className='font-medium'>Contraseña</label>
+                <input type="password" className="w-full border-2 border-gray-400 rounded-xl p-2 mt-1" name="contrasena_actual" id="password"
+                    placeholder="Ingrese su Contraseña" {...register("contra",{
+                        required :{
+                            value: true,
+                            message: "El campo es requerido"
+                        },
+                        minLength:{
+                            value: 8,
+                            message: "contraseña inválida"
+
+                        }
+                    })}/>
+                
+            </div>
+
+            <div className='flex justify-center items-center'>
+                <button type="submit" className="px-4 py-2 text-base rounded-full bg-red-600 text-white hover:bg-red-700">{register ? "Registrate" : "Iniciar sesión"}</button>
+                <h4>{register ? "Si ya tienes cuenta: " : "Si no tienes cuenta:"}
+                    <button onClick={() => setRegister(!register)} className='px-1  text-base rounded-full bg-gray-600 text-white hover:bg-red-700'>{register ? "Inicia sesión" : "Registrate"}</button>
+                </h4>
+
+            </div>
+
+        </form>*/
--- Mecanica Libertad/src/pages/Loginad.jsx
+++ Mecanica Libertad/src/pages/Loginad.jsx
@@ -3,8 +3,8 @@ import Forml from '../components/Forml'
 
 const Loginad = () => {
     return (
-        <section className='h-screen w-full flex items-center justify-center bg-gray-200'>
-            <div className='bg-white px-10 py-14 rounded-3xl shadow-md'>
+        <section className='h-screen w-full flex items-center justify-center bg-red-800'>
+            <div className='bg-white px-10 py-14 max-w-md rounded-3xl shadow-md'>
                 <h1 className='text-center text-3xl font-semibold'>Bienvenido/a</h1>
                 <Forml/>
             </div>
